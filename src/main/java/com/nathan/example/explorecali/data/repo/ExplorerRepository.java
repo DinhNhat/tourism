893a240bc03475fd6f874335418e114aa9565d9d
Nhat Dinh <nguyendinhnat206@gmail.com>
2020-05-30 10:59:33 -0400

    Add a method findByFirstNameLike

--- src/main/java/com/nathan/example/explorecali/data/repo/ExplorerRepository.java
+++ src/main/java/com/nathan/example/explorecali/data/repo/ExplorerRepository.java
@@ -2,8 +2,12 @@ package com.nathan.example.explorecali.data.repo;
 
 import com.nathan.example.explorecali.data.entity.Explorer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ExplorerRepository extends JpaRepository<Explorer, Integer> {
+    Optional<Explorer> findByPersonFirstNameLike(@Param("firstName")String firstName);
 }
