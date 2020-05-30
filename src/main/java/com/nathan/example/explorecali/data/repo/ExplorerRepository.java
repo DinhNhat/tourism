b47df5b8c3a2a554b4f167ca1b61dbf880727250
Nhat Dinh <nguyendinhnat206@gmail.com>
2020-05-30 11:15:16 -0400

    Add method findByCityName in ExplorerRepository Inteface

--- src/main/java/com/nathan/example/explorecali/data/repo/ExplorerRepository.java
+++ src/main/java/com/nathan/example/explorecali/data/repo/ExplorerRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ExplorerRepository extends JpaRepository<Explorer, Integer> {
     Optional<Explorer> findByPersonFirstNameLike(@Param("firstName")String firstName);
+    List<Explorer> findByPersonAddressCityLike(@Param("cityName") String cityName);
 }
